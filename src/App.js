5e4779f1652ed311dcd845f7d2f68a389d06381c
Kidus Mamuye Tekeste <kidusmamuye@gmail.com>
2021-07-17 11:53:59 -0500

    reusable components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+import Todo from "./components/Todo"
+
 function App() {
   return (
     <div>
       <h1>Todos</h1>
-      <div className='card'>
-        <h2>Title</h2>
-        <div className='actions'>
-          <button className='btn'>Delete</button>
-        </div>
-      </div>
+      <Todo />
+      <Todo />
+      <Todo />
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,10 @@
+function Todo(){
+    return <div className='card'>
+    <h2>Title</h2>
+    <div className='actions'>
+      <button className='btn'>Delete</button>
+    </div>
+  </div>;
+} 
+
+export default Todo;
