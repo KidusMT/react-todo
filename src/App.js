87e6050e97e1ea963ff8f2389bc816fbda816134
Kidus Mamuye Tekeste <kidusmamuye@gmail.com>
2021-07-17 00:05:22 -0500

    updated the styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <div>Hello Buddy!</div>;
+  return (
+    <div>
+      <h1>Todos</h1>
+      <div className='card'>
+        <h2>Title</h2>
+        <div>
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
