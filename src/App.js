8522ed4b428cd0198b3829eeacca6dae311dd253
Kidus Mamuye Tekeste <kidusmamuye@gmail.com>
2021-07-18 10:19:42 -0500

    enhanced navigation control

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Switch } from "react-router-dom";
+import MainNavigation from "./components/layout/MainNavigation";
 import AllMeetupPage from "./pages/AllMeetups";
 import FavoritePage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
@@ -6,6 +7,7 @@ import NewMeetupPage from "./pages/NewMeetup";
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact>
           <AllMeetupPage />
---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+function MainNavigation() {
+  return (
+    <header>
+      <div>Logo here</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">New Meetups</Link>
+          </li>
+          <li>
+            <Link to="/favorites">Favorites</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
